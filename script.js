ee52d3385981291b729391ecff759169cfa1536c
Marcellino Pinem <marcellpinem@gmail.com>
2025-08-31 17:16:08 +0700

    inital commit

---
+++ script.js
@@ -0,0 +1,90 @@
+const typingText = document.querySelector(".typing-text p");
+const inputField = document.querySelector(".wrapper .input-field");
+const mistakeTag = document.querySelector(".mistake span");
+const timeTag = document.querySelector(".time span b");
+const wpmTag = document.querySelector(".wpm span");
+const cpmTag = document.querySelector(".cpm span");
+const tryAgainButton = document.querySelector("button");
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = (mistakes = isTyping = 0);
+
+function randomParagraph() {
+  let randomIndex = Math.floor(Math.random() * paragraphs.length);
+  typingText.innerHTML = "";
+
+  paragraphs[randomIndex].split("").forEach((span) => {
+    let spanTag = `<span>${span}</span>`;
+    typingText.innerHTML += spanTag;
+  });
+  typingText.querySelectorAll("span")[0].classList.add("active"); // Perbaikan di sini
+  document.addEventListener("keydown", () => inputField.focus());
+  typingText.addEventListener("click", () => inputField.focus());
+}
+
+function initTyping() {
+  const characters = typingText.querySelectorAll("span");
+  let typedChar = inputField.value.split("")[charIndex];
+
+  if (charIndex < characters.length - 1 && timeLeft > 0) {
+    if (!isTyping) {
+      timer = setInterval(initTimer, 1000);
+      isTyping = true;
+    }
+
+    if (typedChar == null) {
+      charIndex--;
+      characters[charIndex].classList.remove("correct", "incorrect");
+    } else {
+      if (characters[charIndex].innerText === typedChar) {
+        characters[charIndex].classList.add("correct");
+      } else {
+        mistakes++;
+        characters[charIndex].classList.add("incorrect");
+      }
+      charIndex++;
+    }
+
+    characters.forEach((span) => span.classList.remove("active"));
+    characters[charIndex].classList.add("active");
+
+    let wpm = Math.round(((charIndex - mistakes) / 5 / (maxTime - timeLeft)) * 60);
+    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+    mistakeTag.innerText = mistakes;
+    wpmTag.innerText = wpm;
+    cpmTag.innerText = charIndex - mistakes;
+  } else {
+    inputField.value = "";
+    clearInterval(timer);
+  }
+}
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+  } else {
+    clearInterval(timer);
+  }
+}
+
+function resetGame() {
+  randomParagraph();
+  inputField.value = "";
+  clearInterval(timer);
+  timeLeft = maxTime; // Perbaikan di sini
+  charIndex = 0; // Perbaikan di sini
+  mistakes = 0; // Perbaikan di sini
+  isTyping = 0; // Perbaikan di sini
+
+  timeTag.innerText = timeLeft;
+  mistakeTag.innerText = mistakes;
+  wpmTag.innerText = 0;
+  cpmTag.innerText = 0;
+}
+
+randomParagraph();
+inputField.addEventListener("input", initTyping);
+tryAgainButton.addEventListener("click", resetGame);
